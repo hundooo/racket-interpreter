15224f45b5d58e80fd8b82da6dc5dda780b8bde4
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-01-13 17:42:52 -0500

    make token constants static

--- src/token/Token.java
+++ src/token/Token.java
@@ -1,22 +1,22 @@
 package token;
 
 public class Token {
-	public final String ILLEGAL = "ILLEGAL";
-	public final String EOF = "EOF";
+	public static final String ILLEGAL = "ILLEGAL";
+	public static final String EOF = "EOF";
 	
 	// Identifiers + literals
-	public final String IDENT = "IDENT";
-	public final String INT = "INT";
+	public static final String IDENT = "IDENT";
+	public static final String INT = "INT";
 	
 	// Operators 
-	public final String PLUS = "+";
+	public static final String PLUS = "+";
 	
-	public final String LPAREN = "(";
-	public final String RPAREN = ")";
+	public static final String LPAREN = "(";
+	public static final String RPAREN = ")";
 	
 	// Keywords
-	public final String LAMBDA = "LAMBDA";
-	public final String DEFINE = "DEFINE";
+	public static final String LAMBDA = "LAMBDA";
+	public static final String DEFINE = "DEFINE";
 	
 	public String type;
 	public String literal;
