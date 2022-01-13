2cd74b11fd6396e9a313bd74097c9297356d3aff
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-01-13 17:19:12 -0500

    create lexer fields and constructor

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -1,5 +1,13 @@
 package lexer;
 
 public class Lexer {
-
+	
+	public String input;
+	public int position;     // current position in input (points to current char)
+	public int readPosition; // current reading position in input (after current char)
+	public char ch;          // current char under examination
+	
+	public Lexer(String input) { 
+		this.input = input;
+	}
 }
