7e1b05a40217b6b4c57d6c923e4c7748bbde66a2
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-01-13 01:53:44 -0500

    create token object and constants

--- src/token/Token.java
+++ src/token/Token.java
@@ -1,5 +1,30 @@
 package token;
 
 public class Token {
-
+	public final String ILLEGAL = "ILLEGAL";
+	public final String EOF = "EOF";
+	
+	// Identifiers + literals
+	public final String IDENT = "IDENT";
+	public final String INT = "INT";
+	
+	// Operators 
+	public final String PLUS = "+";
+	
+	public final String LPAREN = "(";
+	public final String RPAREN = ")";
+	
+	// Keywords
+	public final String LAMBDA = "LAMBDA";
+	public final String DEFINE = "DEFINE";
+	
+	public String type;
+	public String literal;
+	
+	public Token(String type, String literal) {
+		this.type = type;
+		this.literal = literal;
+	}
+	
+	
 }
