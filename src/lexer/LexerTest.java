0f6914320b47956ff1ed2019a0e019b27be17f9e
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-01-13 18:22:39 -0500

    create new testable racket source code

--- src/lexer/LexerTest.java
+++ src/lexer/LexerTest.java
@@ -4,22 +4,64 @@ import token.Token;
 
 public class LexerTest {
 	public static void main(String[] args) { 
-		String input = "()+";
-		Token[] tests = {new Token("LPAREN", "("), new Token("RPAREN", ")"), new Token("PLUS", "+")};
-		Lexer l = new Lexer();
+		String input = "(define five 5) (define ten 10) (define add (lambda (x y) (+ x y))) (define result (add five ten))";
+		Token[] tests = new Token[35];
+		Lexer l = new Lexer(input);
 		
+		tests[0] = new Token(Token.LPAREN, "(");
+		tests[1] = new Token(Token.DEFINE, "define");
+		tests[2] = new Token(Token.IDENT, "five");
+		tests[3] = new Token(Token.INT, "5");
+		tests[4] = new Token(Token.RPAREN, ")");
+		
+		tests[5] = new Token(Token.LPAREN, "(");
+		tests[6] = new Token(Token.DEFINE, "define");
+		tests[7] = new Token(Token.IDENT, "ten");
+		tests[8] = new Token(Token.INT, "10");
+		tests[9] = new Token(Token.RPAREN, ")");
+		
+		tests[10] = new Token(Token.LPAREN, "(");
+		tests[11] = new Token(Token.DEFINE, "define");
+		tests[12] = new Token(Token.IDENT, "add");
+		tests[13] = new Token(Token.LPAREN, "(");
+		tests[14] = new Token(Token.LAMBDA, "lambda");
+		tests[15] = new Token(Token.LPAREN, "(");
+		tests[16] = new Token(Token.IDENT, "x");
+		tests[17] = new Token(Token.IDENT, "y");
+		tests[18] = new Token(Token.RPAREN, ")");
+		tests[19] = new Token(Token.LPAREN, "(");
+		tests[20] = new Token(Token.PLUS, "+");
+		tests[21] = new Token(Token.IDENT, "x");
+		tests[22] = new Token(Token.IDENT, "y");
+		tests[23] = new Token(Token.RPAREN, ")");
+		tests[24] = new Token(Token.RPAREN, ")");
+		tests[25] = new Token(Token.RPAREN, ")");
+		
+		tests[26] = new Token(Token.LPAREN, "(");
+		tests[27] = new Token(Token.DEFINE, "define");
+		tests[28] = new Token(Token.IDENT, "result");
+		tests[29] = new Token(Token.LPAREN, "(");
+		tests[30] = new Token(Token.IDENT, "add");
+		tests[31] = new Token(Token.IDENT, "five");
+		tests[32] = new Token(Token.IDENT, "ten");
+		tests[33] = new Token(Token.RPAREN, ")");
+		tests[34] = new Token(Token.RPAREN, ")");
+		
+		boolean flag = true;
 		for (int i = 0; i < tests.length; i++) { 
 			Token tt = tests[i];
 			Token tok = l.nextToken();
 			
-			if (tok.type != tt.type) { 
+			if (!tok.type.equals(tt.type)) { 
 				System.out.println("tests[" + i + "]" + "- tokentype wrong. expected=" + tt.type + ", got=" + tok.type);
+				flag = false;
 			}
 			
-			if (tok.literal != tt.literal) { 
+			if (!tok.literal.equals(tt.literal)) { 
 				System.out.println("tests[" + i + "]" + "- literal wrong. expect=" + tt.literal + ", got=" + tok.literal);
+				flag = false;
 			}
 		}
-		
+		System.out.println(flag ? "ALL TESTS PASSED" : "FAIL");
 	}
 }
