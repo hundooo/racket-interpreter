309edd69a53c365d621a59b6e99ac8db4d651795
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-04-26 13:38:58 -0400

    create all interfaces and classes necessary to construct AST

--- src/ast/AST.java
+++ src/ast/AST.java
@@ -1,5 +1,59 @@
 package ast;
 
+import token.Token;
+
 public class AST {
 
 }
+
+interface Node {
+	public String tokenLiteral();
+}
+
+interface Statement extends Node {
+	public void statementNode();
+}
+
+interface Expression extends Node {
+	public void expressionNode();
+}
+
+class Program implements Statement {
+	Statement[] statements;
+
+	public String tokenLiteral() {
+		if (statements.length > 0) {
+			return statements[0].tokenLiteral();
+		} else {
+			return "";
+		}
+	}
+
+	public void statementNode() {}
+}
+
+class DefineStatement implements Statement {
+	Token token;
+	Identifier name;
+	Expression value;
+
+	public String tokenLiteral() {
+		return token.literal;
+	}
+	
+	public void statementNode() {}
+}
+
+class Identifier implements Expression {
+	Token token;
+	String value;
+	
+	public String tokenLiteral() {
+		return token.literal;
+	}
+	
+	public void expressionNode() {}
+}
+
+
+
