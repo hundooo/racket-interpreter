a530b72315144001fd3e22e72d3154c855dba6a8
Zain Muhammad <58492711+100-100@users.noreply.github.com>
2022-04-27 17:02:21 -0400

    add functionality to parseProgram function in parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,6 +1,11 @@
 package parser;
 
+import java.util.ArrayList;
+
+import ast.DefineStatement;
+import ast.Identifier;
 import ast.Program;
+import ast.Statement;
 import lexer.Lexer;
 import token.Token;
 
@@ -21,6 +26,51 @@ public class Parser {
 	}
 	
 	public Program ParseProgram() {
-		return null;
+		Program program = new Program();
+		program.statements = new ArrayList<Statement>();
+		
+		while(curToken.type != Token.EOF) {
+			Statement stmt = parseStatement();
+			if (stmt != null) {
+				program.statements.add(stmt);
+			}
+			nextToken();
+		}
+		
+		return program;
+	}
+	
+	public Statement parseStatement() { 
+		switch(curToken.type) {
+		case Token.DEFINE:
+			return parseDefineStatement();
+		default:
+			return null;
+		}
+	}
+	
+	public Statement parseDefineStatement() {
+		DefineStatement stmt = new DefineStatement(curToken);
+		
+		if (!expectPeek(Token.IDENT)) {
+			return null;
+		}
+		
+		stmt.name = new Identifier(curToken, curToken.literal);
+		
+		return stmt;
+	}
+	
+	public boolean peekTokenIs(String t) {
+		return peekToken.type == t;
+	}
+	
+	public boolean expectPeek(String t) { 
+		if (peekTokenIs(t)) {
+			nextToken();
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
